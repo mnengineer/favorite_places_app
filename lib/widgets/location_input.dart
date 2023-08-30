a88a7bea3f7f003ebe58598bf508c0c85e3784c8
mnengineer <masashiii0115@gmail.com>
2023-08-30 17:50:51 +0900

    feat: Google's Geocoding API 利用

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+
+import 'package:http/http.dart' as http;
 
 class LocationInput extends StatefulWidget {
   const LocationInput({Key? key}) : super(key: key);
@@ -40,11 +44,18 @@ class _LocationInputState extends State<LocationInput> {
     });
 
     locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final lng = locationData.longitude;
+
+    final url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=AIzaSyA4B91LY-0EOx7eRMun564o02mbHMNzPUE');
+    final response = await http.get(url);
+    final data = json.decode(response.body);
+    final address = data['results'][0]['formatted_address'];
+
     setState(() {
       _isGettingLocation = false;
     });
-    print(locationData.latitude);
-    print(locationData.longitude);
   }
 
   @override
