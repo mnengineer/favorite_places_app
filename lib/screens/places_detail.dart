68c742e2945cc5b2e1de5145bf1676f7002d6cca
mnengineer <masashiii0115@gmail.com>
2023-08-30 09:05:10 +0900

    feat: 詳細画面に画像表示

--- lib/screens/places_detail.dart
+++ lib/screens/places_detail.dart
@@ -9,17 +9,18 @@ class PlaceDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(place.title),
-      ),
-      body: Center(
-        child: Text(
-          place.title,
-          style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
+        appBar: AppBar(
+          title: Text(place.title),
         ),
-      ),
-    );
+        body: Stack(
+          children: [
+            Image.file(
+              place.image,
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ],
+        ));
   }
 }
