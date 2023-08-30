4f8b3bca76e71965813e84844fada900829b7c46
mnengineer <masashiii0115@gmail.com>
2023-08-30 23:15:48 +0900

    feat: SQL Databaseからデータ取得

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -7,8 +7,42 @@ import 'package:path/path.dart' as path;
 import 'package:sqflite/sqflite.dart' as sql;
 import 'package:sqflite/sqlite_api.dart';
 
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+    },
+    version: 1,
+  );
+  return db;
+}
+
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
+
+  void loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+    final places = data
+        .map(
+          (item) => Place(
+            id: item['id'] as String,
+            title: item['title'] as String,
+            image: File(item['image'] as String),
+            location: PlaceLocation(
+              latitude: item['lat'] as double,
+              longitude: item['lng'] as double,
+              address: item['address'] as String,
+            ),
+          ),
+        )
+        .toList();
+
+    state = places;
+  }
 
   void addPlace(String title, File image, PlaceLocation location) async {
     final appDir = await syspaths.getApplicationDocumentsDirectory();
@@ -18,14 +52,18 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
     final newPlace =
         Place(title: title, image: copiedImage, location: location);
 
-    final dbPath = await sql.getDatabasesPath();
-    final db = await sql.openDatabase(
-      path.join(dbPath, 'places.db'),
-      onCreate: (db, version) {
-        return db.execute(
-            'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+    final db = await _getDatabase();
+    db.insert(
+      'user_places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path,
+        'lat': newPlace.location.latitude,
+        'lng': newPlace.location.longitude,
+        'address': newPlace.location.address,
       },
-      version: 1,
+      conflictAlgorithm: sql.ConflictAlgorithm.replace,
     );
 
     state = [newPlace, ...state];
